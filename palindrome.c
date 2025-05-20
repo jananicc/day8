2b641f811c8eafec7775fdf4d1a164455c42efde
jananicc <jananic23aid@vetias.ac.in>
2025-05-20 22:04:14 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int isPalindrome(int num) {
+    int original = num;
+    int reversed = 0;
+
+    while (num != 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    return original == reversed;
+}
+
+int main() {
+    int number;
+   
+    scanf("%d", &number);
+
+    if (isPalindrome(number)) {
+        printf("%d is a palindrome.\n", number);
+    } else {
+        printf("%d is not a palindrome.\n", number);
+    }
+
+    return 0;
+}
